eaa7c02cbdbb97c7e726fb2fab00b4cbdbdfbedc
Wei <weijiang1990@gmail.com>
2015-01-24 19:20:33 +0100

    Test read the arborescence of a folder

--- javaconnector/src/test/java/org/openflexo/technologyadapter/java/TestJavaResource.java
+++ javaconnector/src/test/java/org/openflexo/technologyadapter/java/TestJavaResource.java
@@ -1,15 +1,11 @@
 package org.openflexo.technologyadapter.java;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.FileNotFoundException;
 import java.util.Collection;
 
 import org.junit.Assume;
 import org.junit.Test;
-import org.openflexo.foundation.FlexoEditor;
 import org.openflexo.foundation.FlexoException;
-import org.openflexo.foundation.FlexoProject;
 import org.openflexo.foundation.FlexoServiceManager;
 import org.openflexo.foundation.OpenflexoProjectAtRunTimeTestCase;
 import org.openflexo.foundation.resource.DirectoryResourceCenter;
@@ -21,40 +17,43 @@ import org.openflexo.technologyadapter.java.rm.JAVAResource;
 import org.openflexo.technologyadapter.java.rm.JAVAResourceRepository;
 
 public class TestJavaResource extends OpenflexoProjectAtRunTimeTestCase {
-	
+
 	private ModelFactory factory;
-	
-	private static FlexoEditor editor;
-	private static FlexoProject project;
-	
-	private JAVAFolderModel javaRootFolder;
+
+
 	private static JAVATechnologyAdapter adapter;
 	private static FlexoServiceManager applicationContext;
-	
-    @Test
+
+	@Test
 	public void testInitializeServiceManager() throws Exception {
 		applicationContext = instanciateTestServiceManager();
-		adapter = serviceManager.getTechnologyAdapterService().getTechnologyAdapter(JAVATechnologyAdapter.class);
-		resourceCenter = (DirectoryResourceCenter) applicationContext.getResourceCenterService().getResourceCenters().get(0);
+		adapter = serviceManager.getTechnologyAdapterService()
+				.getTechnologyAdapter(JAVATechnologyAdapter.class);
+		resourceCenter = (DirectoryResourceCenter) applicationContext
+				.getResourceCenterService().getResourceCenters().get(0);
 		Assume.assumeNotNull(applicationContext, adapter, resourceCenter);
-		
-		try {
-            this.factory = new ModelFactory(JAVAFolderModel.class);
-        } catch (final ModelDefinitionException e) {
-            System.out.println("error creating factory");
-        }
-		
 
-		JAVAResourceRepository javaResourceRepository = resourceCenter.getRepository(JAVAResourceRepository.class, adapter);
-		Collection<JAVAResource> javaResources = javaResourceRepository.getAllResources();
-		
+		try {
+			this.factory = new ModelFactory(JAVAFolderModel.class);
+		} catch (final ModelDefinitionException e) {
+			System.out.println("error creating factory");
+		}
+
+		JAVAResourceRepository javaResourceRepository = resourceCenter
+				.getRepository(JAVAResourceRepository.class, adapter);
+		Collection<JAVAResource> javaResources = javaResourceRepository
+				.getAllResources();
+
 		for (JAVAResource javaResource : javaResources) {
 			try {
-				JAVAFolderModel javaFolder = javaResource.loadResourceData(null);
-				System.out.println(javaFolder.getFolderModel().getName());
-				javaFolder.getFolderModel().getName();
-//				System.out.println(javaFolder.getChildrenFolders().size());
-//				System.out.println(javaFolder.getChildrenFiles().size());
+				JAVAFolderModel javaFolder = javaResource
+						.loadResourceData(null);
+				System.out.println(javaFolder.getFolderModel().getName()
+						+ "  folder : "
+						+ javaFolder.getChildrenFolders().size()
+						+ "  file : "
+						+ javaFolder.getChildrenFiles().size()
+						);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -65,9 +64,9 @@ public class TestJavaResource extends OpenflexoProjectAtRunTimeTestCase {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 	}
 
 }
